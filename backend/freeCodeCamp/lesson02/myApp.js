c7d75fb3c91d9d2657dbc4a0af9afc7b6e33b237
Mike <mickael.ha@gmail.com>
2024-04-03 16:38:19 +0200

    Basic Node and Express 12/12

--- backend/freeCodeCamp/lesson02/myApp.js
+++ backend/freeCodeCamp/lesson02/myApp.js
@@ -1,7 +1,11 @@
 let express = require("express");
+const { log } = require("fcc-express-bground");
+const bodyParser = require("body-parser");
 let app = express();
 require("dotenv").config();
 absolutePath = __dirname + "/views/index.html";
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(absolutePath);
@@ -13,6 +17,34 @@ app.get("/json", (req, res) => {
   return process.env.MESSAGE_STYLE === "uppercase"
     ? res.json({ message: "Hello json".toUpperCase() })
     : res.json({ message: "Hello json" });
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  const word = req.params.word;
+  res.json({ echo: word });
+});
+
+app.get("/name", (req, res) => {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({ name: `${firstName} ${lastName}` });
+});
+
+app.post("/name", (req, res) => {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  res.json({ name: `${firstName} ${lastName}` });
 });
 
 module.exports = app;
